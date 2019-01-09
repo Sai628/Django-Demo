15b5ab74d5a070ef9318d863a1bd6740874c9acf
Sai628 <sai100628@gmail.com>
2019-01-09 18:27:32 +0800

    first commit

---
+++ polls/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from suit.apps import DjangoSuitConfig
+
+
+class PollsConfig(AppConfig):
+    name = 'polls'
+
+
+class SuitConfig(DjangoSuitConfig):
+    layout = 'horizontal'
